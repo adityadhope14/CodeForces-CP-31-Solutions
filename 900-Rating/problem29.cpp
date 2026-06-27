e2be8d872f74ec8d80d692cf55c6e82a10369fc2
adityadhope14 <adityadhope11@gmail.com>
2026-06-27 21:32:30 +0530

    Add Codeforces 900 Rating Problem 29 solution
    - Added solution for Codeforces 900 Rating Problem 29.
    - Implemented in C++.
    - Time Complexity: O(n)
    - Includes clean and commented code for learning and revision.

---
+++ 900-Rating/problem29.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t ;
+    cin>>t;
+    while(t--){
+        int n ;
+        cin >> n ;
+        vector<int> a(n);
+        for(int i = 0 ; i<n ; i++){
+            cin>>a[i];
+        }
+        bool found = false ;
+        for(int i = 1 ; i<n-1 ; i++){
+            if(a[i]>a[i-1]&&a[i]>a[i+1]){
+                cout<<"yes"<<endl;
+                cout<<i<<" "<<i+1<<" "<<i+2<<endl;
+                found = true;
+                break;
+            }
+            
+        }
+        if(!found){
+            cout<<"no"<<endl;
+        }
+    }
+}       
